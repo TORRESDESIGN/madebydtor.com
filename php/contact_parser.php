9de96aee255e5c5e9a077d5f19c5d86e9b474bde
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2021-03-20 20:59:46 -0700

    edited some PHP

--- php/contact_parser.php
+++ php/contact_parser.php
@@ -1,8 +1,4 @@
 <?php 
-#Remove this error stuff when finished
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
 
   function honeypot_validate($req) {
     if (!empty($req)) {
